3e5f26544038c7d33e3a3353e4f93d494dc8aae7
chaozengHuang <2595467939@qq.com>
2026-06-11 11:08:34 +0800

    提交人：黄朝增 更新内容： 1.更新了获取项目绝对路径的功能，方便不同系统上传图片资源 2.修复了一些bug

--- nexus-frontend/vite.config.js
+++ nexus-frontend/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +12,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      '/api': { target: 'http://localhost:8081', changeOrigin: true },
+      '/post': { target: 'http://localhost:8081', changeOrigin: true },
+      '/board': { target: 'http://localhost:8081', changeOrigin: true },
+      '/comment': { target: 'http://localhost:8081', changeOrigin: true },
+      '/admin': { target: 'http://localhost:8081', changeOrigin: true },
+      '/uploads': { target: 'http://localhost:8081', changeOrigin: true },
+      '/ws': { target: 'ws://localhost:8081', ws: true, changeOrigin: true },
     },
   },
 })
